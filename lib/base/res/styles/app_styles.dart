4c356c81c41680fa0b8a96d64fd45ba3aed8be34
Thejandeera <thejan1010@gmail.com>
2025-08-02 20:42:50 +0530

    done until flights 3.38.10

---
+++ lib/base/res/styles/app_styles.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart'; // ✅ the correct import
+
+Color primary = Color(0xFF687daf);
+
+class AppStyles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0xFF3b3b3b);
+  static Color bgColor = Color(0xFFeeedf2);
+
+  static TextStyle textStyle = TextStyle(
+    fontSize: 16,
+    color: textColor,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle headlineStyle1 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+
+  static TextStyle headlineStyle2 = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+
+  static TextStyle headlineStyle3 = TextStyle(
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+  );
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
+import 'package:ticket_app/base/res/widgets/app_double_text.dart';
+
+import '../base/res/styles/app_styles.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyles.bgColor,
+      //list view is using for scrollable effect
+      body: ListView(
+        children: [
+          SizedBox(height:40),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good Morning !", style: AppStyles.headlineStyle3),
+                        const SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headlineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25,),
+                Container(
+                  padding:  const EdgeInsets.symmetric(horizontal: 12, vertical: 12 ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD)
+                  ),
+                  child: const Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205),),
+                      Text("Search")],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                AppDoubleText(bigText: 'Upcoming Flights', smallText: 'View all',),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
