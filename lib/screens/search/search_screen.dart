3538cd619377be19db8c597231d9212e75261ff8
Thejandeera <thejan1010@gmail.com>
2025-08-08 11:25:58 +0530

    done search page 8:52:17

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -5,6 +5,7 @@ import 'package:ticket_app/base/res/styles/app_styles.dart';
 import 'package:ticket_app/screens/search/widgets/app_text_icon.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 import 'package:ticket_app/screens/search/widgets/find_tickets.dart';
+import 'package:ticket_app/screens/search/widgets/ticket_promotion.dart';
 
 import '../../base/res/widgets/app_double_text.dart';
 import '../../base/utils/all_json.dart';
@@ -43,94 +44,7 @@ class SearchScreen extends StatelessWidget {
             func: () => Navigator.pushNamed(context, AppRoutes.allTickets),
           ),
           const SizedBox(height: 15),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                width: size.width * 0.42,
-                height: 405,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      blurRadius: 1,
-                      spreadRadius: 2,
-                    ),
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(AppMedia.planeSit),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      "20% discount on the early booking of this flight. Don't miss.",
-                      style: AppStyles.headlineStyle2,
-                    ),
-                  ],
-                ),
-              ),
-              Stack(
-                children: [
-                  Container(
-                    width: size.width * 0.44,
-                    height: 210,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 15,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF3AB8B8),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Discount\nfor survey",
-                          style: AppStyles.headlineStyle2.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          "Take the Survey about our services and get discount",
-                          style: AppStyles.headlineStyle2.copyWith(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    right: -45,
-                    top: -40,
-                    child: Container(
-                      padding: EdgeInsets.all(30),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(width: 18, color: AppStyles.circleColor),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          TicketPromotion(),
         ],
       ),
     );
